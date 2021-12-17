dda9a197f989db519b26afd3ddb1003533bf5e31
NKrein <nikokrein@gmail.com>
2021-12-17 17:59:38 -0300

    useState + useEffect (introducción)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
-import Home from './component/Home';
-import NavBar from './component/NavBar';
-import Contador from './container/Contador';
-import List from './container/List';
 
 function App() {
+
+  const [estado, setEstado] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  }
+
+  //Ejecuto en cada actualización (componentWillUpdate)
+  useEffect(() => {
+    console.log("Ejecuto en cada actualización");
+  });
+
+  //Ejecuto unicamente la primera vez (componentWillMount)
+  useEffect(() => {
+    console.log("Ejecuto solo la primera vez");
+  }, []);
+
+  //Ejecuto por cambios de estado de la dependecia ()
+  useEffect(() => {
+    console.log("Ejecuto cuando cambia la dependencia (count)");
+    // Para el cleanUp utilizo el return de la función.
+    // return () => {
+    //   console.log("Acción para cuando se desmonta");
+    // }
+  }, [count]);
+
+  console.log(estado);
+
   return (
     <div className="App">
       <h1>App</h1>
-      <NavBar />
-      <Home saludo={'hola desde App!'} />
-      <List />
-      <Contador stock={5} initial={1}/>
+      <button onClick={() => { setEstado(!estado) }}>cambiar estado</button>
+      <button onClick={add}>sumar count</button>
     </div>
   );
 }
